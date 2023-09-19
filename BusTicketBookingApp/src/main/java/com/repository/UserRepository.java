142ca0c64accaa342a77d2e3fa309445d3f633b1
Atul Jaware Patil <atuljaware.com>
2023-09-20 05:23:26 +0530

    first commit

---
+++ BusTicketBookingApp/src/main/java/com/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.model.Login;
+
+@Repository
+public interface UserRepository extends MongoRepository<Login, String> {
+	Login findByUsername(String username);
+
+}
